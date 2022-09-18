18406c3df971039ddc31414c27d1549ea42305ba
Roman Shapar <romanshapar26@icloud.com>
2022-09-18 15:00:09 +0300

    modif

--- 7.7/cypress/support/commands.js
+++ 7.7/cypress/support/commands.js
@@ -7,12 +7,45 @@
 // commands please read more here:
 // https://on.cypress.io/custom-commands
 // ***********************************************
-const selector = require("../fixtures/selector.json");
+Cypress.Commands.add("createUser", (id, username, firstName, lastName, email, password, phone, userStatus) => {
+  cy.request({
+      method: "POST",
+      url: "https://petstore.swagger.io/v2/user",
+      body: {
+          "id": id,
+          "username": username,
+          "firstName": firstName,
+          "lastName": lastName,
+          "email": email,
+          "password": password,
+          "phone": phone,
+          "userStatus": userStatus
+      }
+  })
+});
 
-Cypress.Commands.add('login', (login, password) => {
-    cy.get(selector.email).type(login);
-    cy.get(selector.password).type(password);
-    cy.get(selector.buttonLoginIn).click();
+Cypress.Commands.add('updateUser', (id, username, firstName, lastName, email, password, phone, userStatus) => {
+  cy.request({
+      method: "PUT",
+      url: "https://petstore.swagger.io/v2/user/Roman",
+      body: {
+          "id": id,
+          "username": username,
+          "firstName": firstName,
+          "lastName": lastName,
+          "email": email,
+          "password": password,
+          "phone": phone,
+          "userStatus": userStatus
+      }
+  })
+});
+
+Cypress.Commands.add('deleteUser', (username) => {
+  cy.request({
+      method: "DELETE",
+      url: "https://petstore.swagger.io/v2/user/Roman",
+  })
 });
 // -- This is a child command --
 // Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
