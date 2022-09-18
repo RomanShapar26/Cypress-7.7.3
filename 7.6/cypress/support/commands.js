55b65ee59e90d8db7ea6d0ce0780d382769c6d87
Roman Shapar <romanshapar26@icloud.com>
2022-09-18 13:52:12 +0300

    modif

--- 7.6/cypress/support/commands.js
+++ 7.6/cypress/support/commands.js
@@ -24,9 +24,29 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
-Cypress.Commands.add("login", (login, password) => {
-  cy.contains("Log in").click();
-  cy.get("#mail").type(login);
-  cy.get("#pass").type(password);
-  cy.contains("Submit").click();
+Cypress.Commands.add('login', (login, password) => {
+  cy.contains('Log in').click();
+  cy.get('#mail').type(login);
+  cy.get('#pass').type(password);
+  cy.contains('Submit').click();
+});
+
+Cypress.Commands.add('logout', () => {
+  cy.contains('Log out').click();
+});
+
+Cypress.Commands.add('addBook', (bookName, author) => {
+  cy.get('.d-flex > div > .btn').click();
+  cy.get('#title').type(bookName);
+  cy.get('#authors').type(author);
+  cy.get('[type="submit"]').click();
+});
+
+Cypress.Commands.add('addFavoriteBook', (bookName, author) => {
+  cy.get('.d-flex > div > .btn').click();
+  cy.get('#title').type(bookName);
+  cy.get('#authors').type(author);
+  cy.get('#favorite').dblclick();
+  cy.get('#favorite').should('be.checked');
+  cy.get('[type="submit"]').click();
 });
